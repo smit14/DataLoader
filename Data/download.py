24e470ecc66145781c037d2b58b9806ad128828d
Smit <sthakkar1407@gmail.com>
2019-11-15 00:25:11 -0800

    download train2014 coco

--- Data/download.py
+++ Data/download.py
@@ -4,4 +4,4 @@ import json
 
 os.system('wget http://images.cocodataset.org/zips/train2014.zip')
 # os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/data/visdial_data.h5')
-os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/data/visdial_params.json')
+# os.system('wget https://filebox.ece.vt.edu/~jiasenlu/codeRelease/visDial.pytorch/data/visdial_params.json')
