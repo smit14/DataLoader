19410c8d9fb76fc7fc46a8154651c1d2d2a58685
Smit <sthakkar1407@gmail.com>
2019-11-15 12:47:36 -0800

    file to test data features are correct

--- Scripts/test_final_features_local.py
+++ Scripts/test_final_features_local.py
@@ -11,10 +11,18 @@ val_feats = f['images_val']
 
 idx_list = [0,5,50,4335,2134]
 
-print('training features...')
-for i in idx_list:
-    print(np.mean(train_feats[i,:,:,:]))
+actual_train_ans = [2.7524679,1.5914612,2.9992323,2.7822866,2.0583184]
+actual_val_ans = [2.4914272,3.1998599,1.7559544,1.7371163,1.766684]
 
+print('training features...')
+idx = 0
+for i in idx_list:
+    print('your ans vs actual ans {} | {}'.format(np.mean(train_feats[i,:,:,:]), actual_train_ans[idx]))
+    idx+=1
+
+idx = 0
 print('validation features...')
 for i in idx_list:
-    print(np.mean(val_feats[i,:,:,:]))
+    print('your ans vs actual ans:{} | {}'.format(np.mean(val_feats[i, :, :, :]), actual_val_ans[idx]))
+    idx+=1
+    # print(np.mean(val_feats[i,:,:,:]))
